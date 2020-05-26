09b7f92f383eb888b6653a3d0504d50068a96cc1
StephanieZou1 <stephanie.zou3@gmail.com>
2020-05-26 11:01:37 -0400

    search

--- src/App.js
+++ src/App.js
@@ -3,14 +3,55 @@ import './App.css'
 import ArticlesContainer from './ArticlesContainer.jsx'
 import SearchArticles from './SearchArticles.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>News For You 📖</h1>
-      <SearchArticles />
-      <ArticlesContainer />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    articles: [],
+    searchTerm: ""
+  }
+
+  handleSearchTerm = (inputFromChild) => {
+    this.setState({
+      searchTerm: inputFromChild
+    })
+  }
+
+  decideWhichArrayToRender = () => {
+    let anArray = this.state.articles.filter((article) => {
+      return article.description.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+        ||
+      article.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    })
+    return anArray
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/articles")
+    .then(r => r.json())
+    .then((newArticles) => {
+      this.setState({
+        articles: newArticles
+      })
+    })
+  }
+
+  render(){
+    console.log("state articles:", this.state.articles)
+    console.log("searchresult:", this.decideWhichArrayToRender())
+
+    return (
+      <div className="App">
+        <h1>News For You 📖</h1>
+        <SearchArticles 
+          searchTerm={this.state.searchTerm}
+          handleSearchTerm={this.handleSearchTerm}
+        />
+        <ArticlesContainer 
+          articles={this.decideWhichArrayToRender()}
+        />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/ArticlesContainer.jsx
+++ src/ArticlesContainer.jsx
@@ -2,23 +2,10 @@ import React, { Component } from 'react'
 import Article from "./Article.jsx"
 
 class ArticlesContainer extends Component {
-  state = {
-    articles: []
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:3000/articles")
-    .then(r => r.json())
-    .then((newArticles) => {
-      this.setState({
-        articles: newArticles
-      })
-    })
-  }
-
+ 
   render() {
-    let articlesArr = this.state.articles.map((article) => {
-      console.log(article)
+    let articlesArr = this.props.articles.map((article) => {
+      // console.log(article)
       return <Article key = {article.id} article = {article} />
     })
 
