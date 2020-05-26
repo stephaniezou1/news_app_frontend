a2441b08b806fbc272774b673ffc8405d968117b
isabel k. lee <hello@isabelklee.co>
2020-05-26 15:35:07 -0400

    updated seaarch bar CSS

--- src/SearchArticles.jsx
+++ src/SearchArticles.jsx
@@ -8,9 +8,11 @@ const SearchArticles = (props) => {
   
 return (
     <div className="search-bar">
+      <label><h3>Search for an article</h3></label>
       <input
         type="text"
         name="searchTerm"
+        placeholder="Start typing an article title, author, or tag"
         value={props.searchTerm}
         onChange={handleWhichInfoToPassUp}
       />
