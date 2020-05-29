e04aff1e40dfd5e6b786ccccb4f29a6e12bba085
isabel k. lee <hello@isabelklee.co>
2020-05-28 21:03:08 -0400

    added delete tag to form

--- src/TagForm.jsx
+++ src/TagForm.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class TagForm extends Component {
+  state = {
+
+  }
+
   render() {
     return (
       <>
@@ -21,6 +25,32 @@ class TagForm extends Component {
           className="new-tag-submit" 
           type="submit" 
           value="Create tag" 
+        />
+      </form>
+
+      <form className="new-tag" onSubmit={this.props.handleDelete}>
+        <h3 className="header">
+          Remove a #tag
+        </h3>
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option selected value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+        {/* <input
+          className="new-tag" 
+          type="text" 
+          name="newTag"
+          value={this.props.newTag} 
+          onChange={this.props.handleChange}
+          autoComplete="off"
+          placeholder="#"
+        /> */}
+        <input
+          className="new-tag-submit" 
+          type="submit" 
+          value="Delete tag" 
         />
       </form>
       </>
